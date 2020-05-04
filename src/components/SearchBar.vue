15e6610312dfb5adab4da1c6ad37d11a7a18f507
pruthvi2103 <pruthvishetty5656@gmail.com>
2020-05-04 23:38:17 +0530

    commited

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="inputfield">
+        <input  @input="onInput" class="" id="vin" />
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onInput: function (event) {
+            this.$emit('termChange',event.target.value);
+            
+        }
+    },
+
+}
+
+</script>
+
+<style scoped>
+
+input{
+    width: 75%;
+}
+
+div{
+    text-align: center;
+    margin: 20px;
+}
+
+</style>
+  
